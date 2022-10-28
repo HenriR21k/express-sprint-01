7624a11e3ba553df94eb47e06408ef6e884c86bb
HenriR21k <k2017731@kingston.ac.uk>
2022-10-28 11:45:37 +0100

    added cors

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 // Imports -----------------------------
 import express from 'express';
 import database from './database.js'
+import cors from 'cors';
+
 // Configure express app ---------------
 const app = new express();
 
 // Configure middleware ----------------
+app.use(function (req, res, next) {
 
+  res.header("Access-Control-Allow-Origin", "*");
+
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+  next();
+
+});
+
+app.use(cors({ origin: '*' }));
 // Controllers -------------------------
 const groupsOfUserController = async (req, res) => {
   const id = req.params.id;
