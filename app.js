97420986eef5529d949261cac00d8946491d5f10
HenriR21k <k2017731@kingston.ac.uk>
2023-01-05 17:01:53 +0000

    refactored getcontrollers

--- app.js
+++ app.js
@@ -143,16 +143,38 @@ const updateTasks = async (sql, id, record) => {
 
 
 
+const buildUsersGroupsSelectSql = (id,variant) => {
+  let sql = '';
+  let table = '`Groupmembers INNER JOIN Groups ON Groupmembers.GroupID=Groups.GroupID ';  
+  let fields = ['Groups.GroupID', 'Groups.GroupName']
+  
+  switch(variant) {
+    default:
+      sql = `SELECT ${fields} FROM ${table}`;
+      if (id) sql += `WHERE Groupmembers.UserID=${id}`
+  }
 
-const groupsOfUserController = async (req, res) => {
+  return sql;
+}
+
+const read = async (selectSql) => {
+  try {
+    const [result] = await database.query(selectSql);
+    return (result.length === 0)
+      ? { isSuccess: false, result: null, message: "No record(s) found" }
+      : { isSuccess: true, result: result, message: "Records successfully recovered" }
+  
+    }
+    catch (error) {
+      return { isSuccess: false, result: null, message: `Failed to execute message ${error.message}` };
+    }
+}
+
+
+const getUserGroupsController = async (req, res) => {
   const id = req.params.id;
   //Build SQL
-  const table = 'Groups ON Groupmembers.GroupID=Groups.GroupID';
-  const whereField = 'Groupmembers.UserID'
-  const fields = ['Groups.GroupID', 'Groups.GroupName']
-  const extendedTable = `Groupmembers INNER JOIN ${table}`;
-  const extendedFields = `${fields}`;
-  const sql = `SELECT ${extendedFields} FROM ${extendedTable} WHERE ${whereField}=${id}`;
+  const sql = buildUsersGroupsSelectSql(id, null);
  
   //Execute Query
   let isSuccess = false;
@@ -174,168 +196,110 @@ const groupsOfUserController = async (req, res) => {
     ? res.status(200).json(result)
     : res.status(400).json({ message })
 };
+
+const buildTasksSelectSql = (id1, id2, variant) => {
+  let sql = '';
+  let fields = ['TaskID','TaskTitle', 'TaskDescription', 'TaskStatus', 'TaskSetDate', 'TaskDeadline'];
+  let table = 'tasks';
+  let extendedTable = '';
+
+  switch(variant) {
+    default:
+      sql = `SELECT ${fields} FROM ${table} WHERE TaskID=${id1}`;
+      break;
+    case 'tasks':
+      sql = `SELECT ${fields} FROM ${table}`
+      break;
+    case 'groupTask':
+      fields = ['tasks.TaskID','tasks.TaskTitle', 'tasks.TaskDescription', 'tasks.TaskStatus', 'tasks.TaskSetDate', 'tasks.TaskDeadline']
+      sql = `SELECT ${fields} FROM ${table} WHERE tasks.GroupID=${id1} and tasks.TaskID=${id2}`
+      break;
+    case 'groupTasks':
+      fields = ['tasks.TaskID','tasks.TaskTitle', 'tasks.TaskDescription', 'tasks.TaskStatus', 'tasks.TaskSetDate', 'tasks.TaskDeadline']
+      sql = `SELECT ${fields} FROM ${table} WHERE tasks.GroupID=${id1}`
+      break;
+    case 'userTasks':
+      table = 'tasks on taskassignment.TaskID=tasks.TaskID';
+      fields = ['tasks.TaskID', 'tasks.TaskTitle']
+      extendedTable = `taskassignment INNER JOIN ${table}`;
+      sql = `SELECT ${fields} FROM ${extendedTable} WHERE tasks.GroupID=${id1} and taskassignment.userID=${id2} and tasks.TaskStatus=\'Outstanding\'`
+
+  }
+
+  return sql;
+}
+
 
 const GroupmemberTasksController = async (req, res) => {
   const id1 = req.params.id1; //Group ID
   const id2 = req.params.id2; //User ID
-  //Build SQL
-  const table = 'tasks on taskassignment.TaskID=tasks.TaskID';
-  const whereField = 'tasks.GroupID';
-  const whereField2 = 'taskassignment.userID';
-  const whereField3 = 'tasks.TaskStatus';
-  const whereCondition = "Outstanding";
-  const fields = ['tasks.TaskID','tasks.TaskTitle']
-  const extendedTable = `taskassignment INNER JOIN ${table}`;
-  const extendedFields = `${fields}`;
-  const sql = `SELECT ${extendedFields} FROM ${extendedTable} WHERE ${whereField}=${id1} and ${whereField2}=${id2} and ${whereField3}=\'Outstanding\'`;
- 
-  //Execute Query
-  let isSuccess = false;
-  let message = "";
-  let result = null;
-  try {
-  [result] = await database.query(sql);
-  if (result.length === 0) message ="No records found";
-  else {
-    isSuccess = true;
-    message = 'Records successfully recovered';
-  }
-  }
-  catch (error) {
-    message = `-Failed to execute message ${error.message}`
-  }
-  //Response
-  isSuccess
-    ? res.status(200).json(result)
-    : res.status(400).json({ message })
+
+  const sql = buildTasksSelectSql(id1, id2, 'userTasks')
+  const { isSuccess, result, message } = await read(sql);
+  if(!isSuccess) return res.status(404).json({message});
+
+  //Response to request
+  res.status(200).json(result);
 };
 
 const getGroupsTasks = async (req, res) => {
-  const id = req.params.id;
-  //Build SQL
-  const table = 'tasks';
-  const whereField = 'tasks.GroupID';
-  const fields = ['tasks.TaskID','tasks.TaskTitle', 'tasks.TaskDescription', 'tasks.TaskStatus', 'tasks.TaskSetDate', 'tasks.TaskDeadline']
-  const sql = `SELECT ${fields} FROM ${table} WHERE ${whereField}=${id}`;
- 
-  //Execute Query
-  let isSuccess = false;
-  let message = "";
-  let result = null;
-  try {
-  [result] = await database.query(sql);
-  if (result.length === 0) message ="No records found";
-  else {
-    isSuccess = true;
-    message = 'Records successfully recovered';
-  }
-  }
-  catch (error) {
-    message = `-Failed to execute message ${error.message}`
-  }
-  //Response
-  isSuccess
-    ? res.status(200).json(result)
-    : res.status(400).json({ message })
+  const id1 = req.params.id;
+  const sql = buildTasksSelectSql(id1, null, 'groupTasks')
+  const { isSuccess, result, message } = await read(sql);
+  if(!isSuccess) return res.status(404).json({message});
+
+  //Response to request
+  res.status(200).json(result);
 };
 
 const getGroupTask = async (req, res) => {
   const id1 = req.params.id1;
   const id2 = req.params.id2;
 
-  //Build SQL
-  const table = 'tasks';
-  const whereField = 'tasks.GroupID';
-  const whereField2 = 'tasks.TaskID';
-  const fields = ['tasks.TaskID','tasks.TaskTitle', 'tasks.TaskDescription', 'tasks.TaskStatus', 'tasks.TaskSetDate', 'tasks.TaskDeadline']
-  const sql = `SELECT ${fields} FROM ${table} WHERE ${whereField}=${id1} and ${whereField2}=${id2}`;
-  
-  //Execute Query
-  let isSuccess = false;
-  let message = "";
-  let result = null;
-  try {
-  [result] = await database.query(sql);
-  if (result.length === 0) message ="No records found";
-  else {
-    isSuccess = true;
-    message = 'Records successfully recovered';
-  }
-  }
-  catch (error) {
-    message = `-Failed to execute message ${error.message}`
-  }
-  //Response
-  isSuccess
-    ? res.status(200).json(result)
-    : res.status(400).json({ message })
+  const sql = buildTasksSelectSql(id1, id2, 'groupTask');
+  const { isSuccess, result, message } = await read(sql);
+  if(!isSuccess) return res.status(404).json({message});
+
+  //Response to request
+  res.status(200).json(result);
 };
 
 const getTasks = async (req, res) => {
-  //Build SQL
-  const fields = ['TaskID','TaskTitle', 'TaskDescription', 'TaskStatus', 'TaskSetDate', 'TaskDeadline']
-  const table = ['tasks']
-  const sql = `SELECT ${fields} FROM ${table}`;
   
-  //Execute Query
-  let isSuccess = false;
-  let message = "";
-  let result = null;
-  try {
-  [result] = await database.query(sql);
-  if (result.length === 0) message ="No records found";
-  else {
-    isSuccess = true;
-    message = 'Records successfully recovered';
-  }
-  }
-  catch (error) {
-    message = `-Failed to execute message ${error.message}`
-  }
-  //Response
-  isSuccess
-    ? res.status(200).json(result)
-    : res.status(400).json({ message })
+  //Validate Request
+
+  const sql = buildTasksSelectSql(null, null, 'tasks')
+  const { isSuccess, result, message } = await read(sql);
+  if(!isSuccess) return res.status(404).json({message});
+
+  //Response to request
+  res.status(200).json(result);
 };
 
 const getTask = async (req, res) => {
-  const id = req.params.id;
-  //Build SQL
-  const fields = ['TaskID','TaskTitle', 'TaskDescription', 'TaskStatus', 'TaskSetDate', 'TaskDeadline']
-  const table = ['tasks']
-  const whereField = ['TaskID']
-  const sql = `SELECT ${fields} FROM ${table} WHERE ${whereField}=${id}`;
-  //Execute Query
-  let isSuccess = false;
-  let message = "";
-  let result = null;
-  try {
-  [result] = await database.query(sql);
-  if (result.length === 0) message ="No records found";
-  else {
-    isSuccess = true;
-    message = 'Records successfully recovered';
-  }
-  }
-  catch (error) {
-    message = `-Failed to execute message ${error.message}`
-  }
-  //Response
-  isSuccess
-    ? res.status(200).json(result)
-    : res.status(400).json({ message })
+  const id1 = req.params.id1;
+
+  //Validate request
+
+  //Access data
+  const sql = buildTasksSelectSql(id1, null, null);
+  const { isSuccess, result, message } = await read(sql);
+  if(!isSuccess) return res.status(404).json({message});
+
+  //Response to request
+  res.status(200).json(result);
 };
 
 // Endpoints ---------------------------
 
 //app.get('/api/groups', groupsController);
-app.get('/api/groups/users/:id', groupsOfUserController); //All groups a user is in
+app.get('/api/groups/users/:id', getUserGroupsController); //All groups a user is in
+
 app.get('/api/groups/:id1/users/:id2/tasks', GroupmemberTasksController); //All tasks assigned to a user in a group
 app.get('/api/groups/:id/tasks', getGroupsTasks); //All tasks belonging to a group
 app.get('/api/groups/:id1/tasks/:id2', getGroupTask) // Get a task belonging to a group
-
 app.get('/api/tasks', getTasks) //All tasks
-app.get('/api/tasks/:id', getTask) //Specific Task
+app.get('/api/tasks/:id1', getTask) //Specific Task
 
 app.post('/api/tasks', postTasksController); //Create a task for a group
 app.put('/api/tasks/:id', putTasksController); //Edit a task in a group
