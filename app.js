dfbebfcefcef8d569758016103cd4a42597036d4
HenriR21k <k2017731@kingston.ac.uk>
2023-04-05 13:42:51 +0100

    Added more controllers to support User Stories

--- app.js
+++ app.js
@@ -104,7 +104,7 @@ const createAssignUserModules = () => {
 
 const createPosts = () => {
     let table = 'posts';
-    let mutableFields = ['UserID', 'TaskID', 'PostTitle', 'PostDescription','PostDate'];
+    let mutableFields = ['UserID', 'TaskID', 'PostDescription','PostDate'];
     return `INSERT INTO ${table} ` + buildSetFields(mutableFields);
   };
 
@@ -117,7 +117,7 @@ const buildTasksUpdateSql = () => {
 const buildModulesUpdateSql = () => {
     let table = 'modules';
     let mutableFields = ['ModuleID', 'ModuleName', 'ModuleDescription', 'ModuleLevel', 'ModuleCode', 'ModuleStartDate', 'ModuleEndDate'];
-    return `UPDATE ${table} ` + buildSetFields(mutableFields) + ` WHERE ModulesID=:ModulesID`;
+    return `UPDATE ${table} ` + buildSetFields(mutableFields) + ` WHERE ModuleID=:ModuleID`;
   };
 
 const create = async (sql,record) => {
@@ -152,6 +152,19 @@ const create = async (sql,record) => {
     catch (error) {
       return { isSuccess: false, result: null, message: `Failed to execute query: ${error.message}` };
     }
+};
+
+const remove = async (sql) => {
+  try {
+    const status = await database.query(sql);
+    return status[0].affectedRows === 0
+      ? { isSuccess: false, result: null, message: "Failed to delete record" }
+      : { isSuccess: true, result: null, message: "Record successfully deleted" }
+    }
+    catch (error) {
+      return { isSuccess: false, result: null, message: `Failed to execute message ${error.message}` };
+    }
+  
 };
 
 const putTasksController = async (req, res) => {
@@ -245,6 +258,34 @@ const updateModules = async (sql, id, record) => {
   }
 };
 
+const updateProjects = async (sql, id, record) => {
+  try {
+    const status = await database.query(sql, { ...record, projectID: id } );
+
+    let isSuccess = false;
+    let message = "";
+    let result = null;
+    try {
+    [result] = await database.query(sql);
+    if (result.length === 0) message ="No records found";
+    else {
+      isSuccess = true;
+      message = 'Records successfully recovered';
+    }
+    }
+    catch (error) {
+      message = `-Failed to execute message ${error.message}`
+    }
+
+    return isSuccess
+      ? { isSuccess: true, result: result, message: 'Record successfully recovered' }
+      : { isSuccess: false, result: null, message: `Failed to recover the inserted record: ${message}` };
+  }
+  catch (error) {
+    return { isSuccess: false, result: null, message: `Failed to execute query: ${error.message}` };
+  }
+};
+
 
 
 const buildUsersGroupsSelectSql = (id,variant) => {
@@ -273,6 +314,20 @@ const read = async (selectSql) => {
     }
 }
 
+const buildGroupsSelectSql = (id,variant) => {
+  let sql = '';
+  let table = 'Groups';  
+  let fields = ['Groups.GroupID', 'Groups.GroupName']
+  
+  switch(variant) {
+    default:
+      sql = `SELECT ${fields} FROM ${table} WHERE Groups.projectID=${id}`;
+      
+  }
+
+  return sql;
+}
+
 
 const getUserGroupsController = async (req, res) => {
   const id = req.params.id;
@@ -284,6 +339,36 @@ const getUserGroupsController = async (req, res) => {
   //Response to request
   res.status(200).json(result);
 };
+
+const getGroupsInProjectsController = async (req, res) => {
+  const id = req.params.id;
+  //Build SQL
+  const sql = buildGroupsSelectSql(id, null, null);
+  const { isSuccess, result, message } = await read(sql);
+  if(!isSuccess) return res.status(404).json({message});
+
+  //Response to request
+  res.status(200).json(result);
+};
+
+const postGroupController = async (req, res) => {
+  // Validate request
+
+  // Access data
+  const sql = createGroup();
+  const { isSuccess, result, message: accessorMessage } = await create(sql,req.body);
+  if (!isSuccess) return res.status(400).json({ message: accessorMessage });
+  
+  // Response to request
+  res.status(201).json(result);
+};
+
+const createGroup = () => {
+  let table = 'groups';
+  let mutableFields = ['GroupID','GroupName','projectID'];
+  return `INSERT INTO ${table} ` + buildSetFields(mutableFields);
+};
+
 
 const buildTasksSelectSql = (id1, id2, variant) => {
   let sql = '';
@@ -323,6 +408,11 @@ const buildTasksSelectSql = (id1, id2, variant) => {
       extendedTable = `taskassignment INNER JOIN ${table}`
       sql = `SELECT ${fields} FROM ${extendedTable} WHERE taskassignment.TaskID=${id1}`
       break;
+    case 'isAssigned':
+      sql = `SELECT TaskUserID 
+              FROM taskassignment 
+              INNER JOIN tasks ON tasks.TaskID=taskassignment.TaskID
+              WHERE taskassignment.TaskID = ${id1} AND taskassignment.UserID = ${id2}`
   }
 
   return sql;
@@ -330,7 +420,7 @@ const buildTasksSelectSql = (id1, id2, variant) => {
 
 const buildPostsSelectSQL = (id1, id2, variant) => {
   let sql = '';
-  let fields = ['posts.PostID, posts.UserID, posts.TaskID, posts.PostTitle, posts.PostDescription, posts.PostDate'];
+  let fields = ['posts.PostID, posts.UserID, posts.TaskID, posts.PostDescription, posts.PostDate'];
   let table = 'posts';
   let extendedTable = '';
 
@@ -341,13 +431,61 @@ const buildPostsSelectSQL = (id1, id2, variant) => {
     case 'TaskPosts':
       table = 'tasks on posts.TaskID=tasks.TaskID'
       extendedTable = `posts INNER JOIN ${table}`;
-      sql = `SELECT ${fields} FROM ${extendedTable} WHERE posts.TaskID = ${id1}`
+      sql = `SELECT ${fields} FROM ${extendedTable} WHERE posts.TaskID = ${id1} ORDER BY posts.PostDescription ASC;`
       break;
   }
 
   return sql;
 
 }
+
+const getIsUserAssignedToTaskController = async (req, res) => {
+  const id1 = req.params.id1;
+  const id2 = req.params.id2;
+  const sql = buildTasksSelectSql(id1, id2, 'isAssigned')
+  const { isSuccess, result, message } = await read(sql);
+  if(!isSuccess) return res.status(404).json({message});
+
+  //Response to request
+  res.status(200).json(result);
+};
+
+const postAssignUserToTaskController = async (req, res) => {
+  // Validate request
+
+  // Access data
+  const sql = createAssign();
+  const { isSuccess, result, message: accessorMessage } = await create(sql,req.body);
+  if (!isSuccess) return res.status(400).json({ message: accessorMessage });
+  
+  // Response to request
+  res.status(201).json(result);
+};
+
+const createAssign = () => {
+  let table = 'taskassignment';
+  let mutableFields = ['UserID','TaskID'];
+  return `INSERT INTO ${table} ` + buildSetFields(mutableFields);
+};
+
+const removeAssignedUserFromTaskController = async (req, res) => {
+  // Validate request
+  const id = req.params.id;
+  
+  // Access data
+  const sql = removeAssign(id);
+  const { isSuccess, result, message: accessorMessage } = await remove(sql);
+  if (!isSuccess) return res.status(400).json({ message: accessorMessage });
+  
+  // Response to request
+  res.status(200).json(result);
+  
+}
+
+const removeAssign = (id) => {
+  let table = 'taskassignment';
+  return `DELETE from ${table} WHERE TaskUserID=${id}`;
+};
 
 
 const GroupmemberTasksController = async (req, res) => {
@@ -430,11 +568,22 @@ const getUsersAssignedToTask = async (req, res) => {
   res.status(200).json(result);
 };
 
+const getGroupsUsersController = async (req, res) => {
+  const id1 = req.params.id1;
+  const sql = buildUsersSelectSql(id1, null, 'GroupsUsers')
+  const { isSuccess, result, message } = await read(sql);
+  if(!isSuccess) return res.status(404).json({message});
+
+  //Response to request
+  res.status(200).json(result);
+};
+
 const buildUsersSelectSql = (id1, id2, variant) => {
   let sql = '';
   let fields = ['firstName, lastName'];
   let table = 'Users';
   let extendedTable = '';
+  let join = ''
 
   switch(variant) {
     default:
@@ -445,6 +594,12 @@ const buildUsersSelectSql = (id1, id2, variant) => {
       extendedTable = `usertype INNER JOIN ${table}`;
       fields = ['UserTypeName']
       sql = `SELECT ${fields} FROM ${extendedTable} WHERE users.UserID = ${id1}`
+      break;
+    case "GroupsUsers":
+      table = "groupmembers INNER JOIN users ON groupmembers.UserID=users.UserID"
+      sql = `SELECT Users.UserID ,firstName, lastName FROM ${table} WHERE groupmembers.GroupID = ${id1}`
+
+
   }
 
   return sql;
@@ -502,6 +657,74 @@ const getUserUserTypeController = async (req, res) => {
   res.status(200).json(result);
 }
 
+const buildProjectsSelectSQL = (id1, id2, variant) => {
+  let sql = '';
+  let fields = ['projectID, projectName, projectDescription, projectStartDate, projectEndDate'];
+  let table = 'projects';
+  let extendedTable = '';
+
+  switch(variant) {
+    default:
+      sql = `SELECT ${fields} FROM ${table} WHERE ModuleID=${id1}`;
+      break;
+      
+  }
+
+  return sql;
+
+}
+
+const getProjectController = async (req, res) => {
+  
+  const id1 = req.params.id
+  //Validate Request
+
+  const sql = buildProjectsSelectSQL(id1, null, null)
+  const { isSuccess, result, message } = await read(sql);
+  if(!isSuccess) return res.status(404).json({message});
+
+  //Response to request
+  res.status(200).json(result);
+};
+
+const postProjectController = async (req, res) => {
+  // Validate request
+
+  // Access data
+  const sql = createProject();
+  const { isSuccess, result, message: accessorMessage } = await create(sql,req.body);
+  if (!isSuccess) return res.status(400).json({ message: accessorMessage });
+  
+  // Response to request
+  res.status(201).json(result);
+};
+
+const createProject = () => {
+  let table = 'projects';
+  let mutableFields = ['projectID', 'projectName', 'projectDescription', 'projectStartDate', 'projectEndDate', 'ModuleID'];
+  return `INSERT INTO ${table} ` + buildSetFields(mutableFields);
+};
+
+const putProjectController = async (req, res) => {
+  // Validate request
+  const id = req.params.id;
+  const record = req.body;
+
+  // Access data
+  const sql = buildProjectUpdateSql();
+  const { isSuccess, result, message: accessorMessage } = await updateProjects(sql, id, record);
+  if (!isSuccess) return res.status(400).json({ message: accessorMessage });
+  
+  // Response to request
+  res.status(200).json(result);
+  
+}
+
+const buildProjectUpdateSql = () => {
+  let table = 'projects';
+  let mutableFields = ['projectID', 'projectName', 'projectDescription', 'projectStartDate', 'projectEndDate', 'ModuleID'];
+  return `UPDATE ${table} ` + buildSetFields(mutableFields) + ` WHERE projectID=:projectID`;
+};
 
 
 
@@ -509,13 +732,19 @@ const getUserUserTypeController = async (req, res) => {
 
 //app.get('/api/groups', groupsController);
 app.get('/api/groups/users/:id', getUserGroupsController); //All groups a user is in
-app.get('/api/tasks/:id1/users', getUsersAssignedToTask); 
+app.get('/api/tasks/:id1/users', getUsersAssignedToTask);
+app.get('/api/groups/:id1/users', getGroupsUsersController);
+app.get('/api/projects/:id1/groups', getGroupsInProjectsController) //get all groups for a project
+app.post('/api/groups', postGroupController) //create a group for a project
 
 app.get('/api/users/:id1', getUserById); //Gets a specific user
 app.get('/api/users/:id1/userType', getUserUserTypeController) //get users user type.
 app.post('/api/users/modules', postUserModulesController)
 
 app.get('/api/groups/:id1/users/:id2/tasks', GroupmemberTasksController); //All tasks assigned to a user in a group
+app.get('/api/task/:id1/users/:id2', getIsUserAssignedToTaskController); //Checks to see if user has been assigned to a task
+app.post('/api/assignTask', postAssignUserToTaskController);
+app.delete('/api/unAssignTask/:id', removeAssignedUserFromTaskController); 
 app.get('/api/groups/:id/tasks', getGroupsTasks); //All tasks belonging to a group
 app.get('/api/tasks/:id2/groups/:id1', getGroupTask) // Get a task belonging to a group
 app.get('/api/tasks', getTasks) //All tasks
@@ -525,13 +754,16 @@ app.post('/api/tasks', postTasksController); //Create a task for a group
 app.put('/api/tasks/:id', putTasksController); //Edit a task in a group
 app.post('/api/users/tasks', postTaskAssignmentController); //Assign user to task
 
-
 app.get('/api/tasks/:id1/posts', getTaskPosts);
-app.post('/api/tasks/posts', postFeedbackController);
+app.post('/api/tasks/posts', postFeedbackController); //Feedback for tasks
 
 app.get('/api/modules', getModulesController);
-app.post('/api/modules', postModulesController);
+app.post('/api/module', postModulesController);
 app.put('/api/modules/:id', putModulesController);
+
+app.get('/api/modules/:id/projects', getProjectController)
+app.post('/api/projects', postProjectController)
+app.put('/api/projects/:id', putProjectController)
 
 
 
